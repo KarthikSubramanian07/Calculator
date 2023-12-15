ba354d20edd7046a2630d84625d5132267a3420b
pieman909 <66752243+pieman909@users.noreply.github.com>
2023-12-15 10:21:36 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 # importing everyting from tkinter
 from tkinter import *
+import customtkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
 
 # expression to access among all the functions
 expression = ""
@@ -37,196 +41,151 @@ def evaluate(equation):
 
 def main():
     # main window
-    window = Tk()
+    window = customtkinter.CTk()
     # setting the title of GUI window
     window.title("Calculator")
     # set the configuration of GUI window
-    window.geometry("390x200")
+    window.geometry("600x220")
     # varible class instantiation
     equation = StringVar()
     # input field for the expression
-    input_field = Entry(window, textvariable=equation)
-    input_field.place(height=100)
+    input_field =  customtkinter.CTkEntry(master=window,
+                               placeholder_text="Enter the Expression",
+                               textvariable = equation,
+                               width=120,
+                               height=25,
+                               border_width=2,
+                               corner_radius=10,)
     # we are using grid position
     # for the arrangement of the widgets
-    input_field.grid(columnspan=4, ipadx=100, ipady=5)
-    # settin the placeholder message for users
-    equation.set("Enter the expression")
-    # creating buttons and placing them at respective positions
-    _1 = Button(
-        window,
+    input_field.grid(columnspan=4, ipadx=100, ipady=10)
+    # creating buttons and placing them at respective positions, origin being (0,0)
+    
+    #Button for 1, the declaration below is attached to the object and sets its position
+    _1 = customtkinter.CTkButton(
         text="1",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(1, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _1.grid(row=2, column=0)
-    _2 = Button(
-        window,
+    _1.grid(row=2, column=0,pady=5, padx=5)
+    
+    _2 = customtkinter.CTkButton(
+       
         text="2",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(2, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _2.grid(row=2, column=1)
-    _3 = Button(
-        window,
+    _2.grid(row=2, column=1, pady=5, padx=5,)
+    
+    _3 = customtkinter.CTkButton(
+       
         text="3",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(3, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _3.grid(row=2, column=2)
-    _4 = Button(
-        window,
+    _3.grid(row=2, column=2, pady=5, padx=5,)
+    
+    _4 = customtkinter.CTkButton(
+       
         text="4",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(4, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _4.grid(row=3, column=0)
-    _5 = Button(
-        window,
+    _4.grid(row=3, column=0, pady=5, padx=5,)
+    
+    _5 = customtkinter.CTkButton(
+       
         text="5",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(5, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _5.grid(row=3, column=1)
-    _6 = Button(
-        window,
+    _5.grid(row=3, column=1, pady=5, padx=5,)
+    
+    _6 = customtkinter.CTkButton(
+       
         text="6",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(6, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _6.grid(row=3, column=2)
-    _7 = Button(
-        window,
+    _6.grid(row=3, column=2, pady=5, padx=5,)
+    
+    _7 = customtkinter.CTkButton(
+       
         text="7",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(7, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _7.grid(row=4, column=0)
-    _8 = Button(
-        window,
+    _7.grid(row=4, column=0, pady=5, padx=5,)
+    
+    _8 = customtkinter.CTkButton(
+       
         text="8",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(8, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _8.grid(row=4, column=1)
-    _9 = Button(
-        window,
+    _8.grid(row=4, column=1, pady=5, padx=5,)
+    
+    _9 = customtkinter.CTkButton(
+       
         text="9",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(9, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _9.grid(row=4, column=2)
-    _0 = Button(
-        window,
+    _9.grid(row=4, column=2, pady=5, padx=5,)
+    
+    _0 = customtkinter.CTkButton(
+       
         text="0",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number(0, equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    _0.grid(row=5, column=0)
-    plus = Button(
-        window,
+    _0.grid(row=5, column=0, pady=5, padx=5,)
+    
+    plus = customtkinter.CTkButton(
+       
         text="+",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number("+", equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    plus.grid(row=2, column=3)
-    minus = Button(
-        window,
+    plus.grid(row=2, column=3, pady=5, padx=5,)
+    
+    minus = customtkinter.CTkButton(
+       
         text="-",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number("-", equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    minus.grid(row=3, column=3)
-    multiply = Button(
-        window,
+    minus.grid(row=3, column=3, pady=5, padx=5,)
+    
+    multiply = customtkinter.CTkButton(
+       
         text="*",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number("*", equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    multiply.grid(row=4, column=3)
-    divide = Button(
-        window,
+    multiply.grid(row=4, column=3, pady=5, padx=5,)
+    
+    divide = customtkinter.CTkButton(
+       
         text="/",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: input_number("/", equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    divide.grid(row=5, column=3)
-    equal = Button(
-        window,
+    divide.grid(row=5, column=3, pady=5, padx=5,)
+    
+    equal = customtkinter.CTkButton(
+       
         text="=",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: evaluate(equation),
-        height=2,
-        width=7,
+        master=window,
     )
-    equal.grid(row=5, column=2)
-    clear = Button(
-        window,
+    equal.grid(row=5, column=2, pady=5, padx=5,)
+    
+    clear = customtkinter.CTkButton(
+       
         text="Clear",
-        fg="black",
-        # bg="black",
-        bd=0,
         command=lambda: clear_input_field(equation),
-        height=2,
-        width=7,
+        master=window,
     )
     clear.grid(row=5, column=1)
     # showing the GUI
